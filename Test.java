8d15f3a3174d9a53a25922c24ec5435317e6c2fd
Egert Jõesaar <egert.joesaar@tigu.hk.tlu.ee>
2017-03-01 11:10:09 +0200

    made more changes

--- Test.java
+++ Test.java
@@ -1,10 +1,13 @@
 import java.util.*;
 public class Test{
 	public static void main(String[] arg){
-		Kindad k1=new Kindad(2, 3, 1.5);
+		Kindad k1=new Kindad(12, 3, 1.5);
+		Kindad k2=new Kindad(12, 3, 1.5);
 		List<LisaHind> kasitoo=new ArrayList<LisaHind>();
 		kasitoo.add(k1);
-		kasitoo.add(new LapiTekk(3, 4, 5, 1.13, 1.5));
+		kasitoo.add(k2);
+		kasitoo.add(new LapiTekk(13, 4, 5, 1.13, 1.5));
+		kasitoo.add(new LapiTekk(13, 4, 5, 1.13, 1.5));
 		for(LisaHind l: kasitoo){
 			System.out.println(l.MyygiHind());
 		}
