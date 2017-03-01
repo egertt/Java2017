6dedb7ef857bfedc005d9a5ce2085ac9cde06940
Egert Jõesaar <egert.joesaar@tigu.hk.tlu.ee>
2017-03-01 11:35:14 +0200

    muutused

--- Test.java
+++ Test.java
@@ -2,12 +2,12 @@ import java.util.*;
 public class Test{
 	public static void main(String[] arg){
 		Sokid s1=new Sokid(12, 3, 1.5);
-		Sokid s2=new Sokid(12, 3, 1.5);
+		
 		List<LisaHind> kasitoo=new ArrayList<LisaHind>();
 		kasitoo.add(s1);
-		kasitoo.add(s2);
+		
 		kasitoo.add(new VatiTekk(13, 4, 5, 1.13, 1.5));
-		kasitoo.add(new VatiTekk(13, 4, 5, 1.13, 1.5));
+		
 		for(LisaHind l: kasitoo){
 			System.out.println("Müügihind on=" + l.MyygiHind());
 
